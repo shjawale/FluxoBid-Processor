bd822a9af013f3025dea372d4092f99eeb8ac3da
Shruti Jawale <72288432+shjawale@users.noreply.github.com>
2026-05-10 08:37:41 -0700

    Update test_bidder.cpp

--- tests/test_bidder.cpp
+++ tests/test_bidder.cpp
@@ -7,13 +7,6 @@ uint32_t factorial( uint32_t number ) {
     return number <= 1 ? number : factorial(number-1) * number;
 }
 
-TEST_CASE( "Factorials are computed", "[factorial]" ) {
-    REQUIRE( factorial( 1) == 1 );
-    REQUIRE( factorial( 2) == 2 );
-    REQUIRE( factorial( 3) == 6 );
-    REQUIRE( factorial(10) == 3'628'800 );
-}
-
 TEST_CASE( "Parse a JSON request", "[request]" ) {
     std::string_view json_raw = R"(
         \"id\": \"fluxo-test-req-001\",\\
